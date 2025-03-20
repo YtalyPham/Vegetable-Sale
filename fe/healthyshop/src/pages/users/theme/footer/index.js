6ba20488dc3b9cb9429a911dd26d61ea79908dce
YtalyPham <nhuyphamdev@gmail.com>
2025-03-20 14:08:50 +0700

    Handle footer basic

--- fe/healthyshop/src/pages/users/theme/footer/index.js
+++ fe/healthyshop/src/pages/users/theme/footer/index.js
@@ -1,8 +1,78 @@
 import { memo } from "react";
- 
+import "./style.scss";
+import { Link } from "react-router-dom";
+import { AiOutlineFacebook, AiOutlineInstagram, AiOutlineLinkedin } from "react-icons/ai";
 
 const Footer = () =>{
-    return <h1>Footer</h1>
+    return <footer className="footer">
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-3">
+                    <div className="footer__about">
+                        <h1 className="footer__about__logo">Healthy Shop</h1>
+                        <ul>
+                            <li>Địa chỉ: 100 Trường Chinh</li>
+                            <li>Phone: 0933 999 878</li>
+                            <li>Email: healthyShop@gmail.com</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="col-lg-6">
+                    <div className="footer__widget">
+                        <h6>Cửa hàng</h6>
+                        <ul>
+                            <li>
+                                <Link to="">Liên hệ</Link>
+                            </li>
+                            <li>
+                                <Link to="">Thông tin về chúng tôi</Link>
+                            </li>
+                            <li>
+                                <Link to="">Sản phẩm kinh doanh</Link>
+                            </li>
+                        </ul>
+                        <ul>
+                            <li>
+                                <Link to="">Thông tin tài khoản</Link>
+                            </li>
+                            <li>
+                                <Link to="">Giỏ hàng</Link>
+                            </li>
+                            <li>
+                                <Link to="">Danh sách ưa thích</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="col-lg-3">
+                    <div className="footer__widget">
+                        <h6>Khuyến mãi & ưu đai4</h6>
+                        <p>Đăng ký nhận thông tin tại đây</p>
+                        <form action="#">
+                            <div>
+                                <input type="text" placeholder="Nhập email"/>
+                                <button type="submit" className="button-submit">Đăng ký</button>
+            
+                            </div>
+                            <div className="footer__widget__social">
+                                <div>
+                                    <AiOutlineFacebook/>
+                                </div>
+                                <div>
+                                    <AiOutlineInstagram/>
+                                </div>
+                                <div>
+                                    <AiOutlineLinkedin/>
+                                </div>
+
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>    
+
+    </footer>
 
 };
 
