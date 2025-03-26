8b794720dac8802a5f04a33b779cfce7953d1bc6
YtalyPham <nhuyphamdev@gmail.com>
2025-03-26 17:13:08 +0700

    Handle productsPage p1

---
+++ fe/healthyshop/src/pages/users/productsPage/index.js
@@ -0,0 +1,14 @@
+import { memo } from "react";
+import "./style.scss";
+import Breadcrumb from "../theme/breadcrumb";
+ 
+const ProductsPage = () =>{
+    return  <>
+        <Breadcrumb name="Danh sách sản phẩm"/>
+    </>
+       
+     
+           
+};
+
+export default memo(ProductsPage);
