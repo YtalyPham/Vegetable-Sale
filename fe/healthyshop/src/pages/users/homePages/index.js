f8041c30efb70a0cf9e76f678e9600c8cf190f1a
YtalyPham <nhuyphamdev@gmail.com>
2025-03-28 17:23:00 +0700

    Handle ProductDetailPage p2

--- fe/healthyshop/src/pages/users/homePages/index.js
+++ fe/healthyshop/src/pages/users/homePages/index.js
@@ -8,20 +8,14 @@ import c2 from "assets/users/images/categories/c2.jpg";
 import c3 from "assets/users/images/categories/c3.jpg";
 import c4 from "assets/users/images/categories/c4.jpg";
 import c5 from "assets/users/images/categories/c5.jpg";
-import f1 from "assets/users/images/featured/f1.jpg";
-import f2 from "assets/users/images/featured/f2.jpg";
-import f3 from "assets/users/images/featured/f3.jpg";
-import f4 from "assets/users/images/featured/f4.jpg";
-import f5 from "assets/users/images/featured/f5.jpg";
-import f6 from "assets/users/images/featured/f6.jpg";
-import f7 from "assets/users/images/featured/f7.jpg";
-import f8 from "assets/users/images/featured/f8.jpg";
+
 import banner1 from "assets/users/images/banner/banner1.jpg";
 import banner2 from "assets/users/images/banner/banner2.jpg";
 import "./style.scss";
 
 
 import {ProductCard} from "../../../component";
+import { featproducts } from "../../../utils/common";
 
 const HomePages = () => {
   const responsive = {
@@ -66,108 +60,7 @@ const HomePages = () => {
     },
   ];
 
-  const featproducts = {
-    all: {
-      title: "Toàn bộ",
-      products: [
-        {
-          img: f1,
-          name: "Thịt bò nạt",
-          price: 20000,
-        },
-        {
-          img: f2,
-          name: "Chuối",
-          price: 17800,
-        },
-        {
-          img: f3,
-          name: "Ổi",
-          price: 25000,
-        },
-        {
-          img: f4,
-          name: "Dưa hấu",
-          price: 44000,
-        },
-        {
-          img: f5,
-          name: "Nho tím",
-          price: 120000,
-        },
-        {
-          img: f6,
-          name: "Hamburger",
-          price: 86000,
-        },
-        {
-          img: f7,
-          name: "Xoài",
-          price: 69000,
-        },
-        {
-          img: f8,
-          name: "Táo Úc",
-          price: 53000,
-        },
-      ],
-    },
-    freshMeat: {
-      title: "Thịt tươi",
-      products: [
-        {
-          img: f1,
-          name: "Thịt tươi nè",
-          price: 20000,
-        },
-      ],
-    },
-    fruits: {
-      title: "Trái cây",
-      products: [
-        {
-          img: f2,
-          name: "Chuối",
-          price: 17800,
-        },
-        {
-          img: f3,
-          name: "Ổi",
-          price: 25000,
-        },
-        {
-          img: f4,
-          name: "Dưa hấu",
-          price: 44000,
-        },
-        {
-          img: f5,
-          name: "Nho tím",
-          price: 120000,
-        },
-        {
-          img: f7,
-          name: "Xoài",
-          price: 69000,
-        },
-        {
-          img: f8,
-          name: "Táo Úc",
-          price: 53000,
-        },
-      ],
-    },
-    fastfood: {
-      title: "Thức ăn nhanh",
-      products: [
-        {
-          img: f6,
-          name: "Hamburger",
-          price: 86000,
-        },
-      ],
-    },
-  };
+ 
   const renderFeaturedProducts = (data) => {
     const tabList = [];
     const tabPanels = [];
---
+++ fe/healthyshop/src/utils/common.js
@@ -0,0 +1,111 @@
+import f1 from "assets/users/images/featured/f1.jpg";
+import f2 from "assets/users/images/featured/f2.jpg";
+import f3 from "assets/users/images/featured/f3.jpg";
+import f4 from "assets/users/images/featured/f4.jpg";
+import f5 from "assets/users/images/featured/f5.jpg";
+import f6 from "assets/users/images/featured/f6.jpg";
+import f7 from "assets/users/images/featured/f7.jpg";
+import f8 from "assets/users/images/featured/f8.jpg";
+
+export const featproducts = {
+    all: {
+      title: "Toàn bộ",
+      products: [
+        {
+          img: f1,
+          name: "Thịt bò nạt",
+          price: 20000,
+        },
+        {
+          img: f2,
+          name: "Chuối",
+          price: 17800,
+        },
+        {
+          img: f3,
+          name: "Ổi",
+          price: 25000,
+        },
+        {
+          img: f4,
+          name: "Dưa hấu",
+          price: 44000,
+        },
+        {
+          img: f5,
+          name: "Nho tím",
+          price: 120000,
+        },
+        {
+          img: f6,
+          name: "Hamburger",
+          price: 86000,
+        },
+        {
+          img: f7,
+          name: "Xoài",
+          price: 69000,
+        },
+        {
+          img: f8,
+          name: "Táo Úc",
+          price: 53000,
+        },
+      ],
+    },
+    freshMeat: {
+      title: "Thịt tươi",
+      products: [
+        {
+          img: f1,
+          name: "Thịt tươi nè",
+          price: 20000,
+        },
+      ],
+    },
+    fruits: {
+      title: "Trái cây",
+      products: [
+        {
+          img: f2,
+          name: "Chuối",
+          price: 17800,
+        },
+        {
+          img: f3,
+          name: "Ổi",
+          price: 25000,
+        },
+        {
+          img: f4,
+          name: "Dưa hấu",
+          price: 44000,
+        },
+        {
+          img: f5,
+          name: "Nho tím",
+          price: 120000,
+        },
+        {
+          img: f7,
+          name: "Xoài",
+          price: 69000,
+        },
+        {
+          img: f8,
+          name: "Táo Úc",
+          price: 53000,
+        },
+      ],
+    },
+    fastfood: {
+      title: "Thức ăn nhanh",
+      products: [
+        {
+          img: f6,
+          name: "Hamburger",
+          price: 86000,
+        },
+      ],
+    },
+  };
