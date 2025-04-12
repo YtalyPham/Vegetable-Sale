f4435e49aba35afc2dcd71d30c640b9181f9c3af
YtalyPham <nhuyphamdev@gmail.com>
2025-04-12 22:49:14 +0700

    Handle CheckoutPage p1

--- fe/healthyshop/src/utils/router.js
+++ fe/healthyshop/src/utils/router.js
@@ -4,6 +4,7 @@ export const ROUTERS={
         PROFILE:"profile",
         PRODUCTS: "/san-pham",
         PRODUCT: "/san-pham/chi-tiet/:id",
-        SHOPPING_CART: "/gio-hang"
+        SHOPPING_CART: "/gio-hang",
+        CHECKOUT: "/thanh-toan",
     },
 }
