947ed8510e41edcce883023cc0f6358720348a4a
YtalyPham <nhuyphamdev@gmail.com>
2025-04-13 18:29:58 +0700

    Handle OrderAdPage p1

---
+++ fe/healthyshop/src/pages/admin/theme/header/index.js
@@ -0,0 +1,46 @@
+import { memo } from "react";
+import { ROUTERS } from "../../../../utils/router";
+import { useLocation, useNavigate } from "react-router-dom";
+import { AiOutlineLogout, AiOutlineShoppingCart } from "react-icons/ai";
+import "./style.scss";
+const HeaderAd = ({ children, ...props }) => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const navItems = [
+    {
+      Path: ROUTERS.ADMIN.ORDERS,
+      onClick: () => navigate(ROUTERS.ADMIN.ORDERS),
+      label: "Đặt hàng",
+      icon: <AiOutlineShoppingCart />,
+    },
+    {
+      Path: ROUTERS.ADMIN.LOGOUT,
+      onClick: () => {},
+      label: "Đăng xuất",
+      icon: <AiOutlineLogout />,
+    },
+  ];
+  return (
+    <div className="admin__header container" {...props}>
+      <nav className="admin__header__nav">
+        {navItems.map(({ path, onClick, label, icon }) => (
+          <div
+            key={path}
+            className={`admin__header__nav__item 
+          ${
+            location.pathname.includes(path)
+              ? "admin__header__nav__item--active"
+              : ""
+          }`}
+            onClick={onClick}
+          >
+            <span className="admin__header__nav-icon">{icon}</span>
+            <span>{label}</span>
+          </div>
+        ))}
+      </nav>
+    </div>
+  );
+};
+
+export default memo(HeaderAd);
--- fe/healthyshop/src/utils/router.js
+++ fe/healthyshop/src/utils/router.js
@@ -10,6 +10,7 @@ export const ROUTERS={
     },
     ADMIN: {
         LOGIN: `${ADMIN_PATH}/dang-nhap`,
+        LOGOUT: `${ADMIN_PATH}/dang-xuat`,
         ORDERS: `${ADMIN_PATH}/dat-hang`,
        
     },
