2fcb3c6a3abe1f317481d50ff296c178dbc2ecaf
YtalyPham <nhuyphamdev@gmail.com>
2025-04-13 14:01:23 +0700

    Handle Router Admin management p1

--- fe/healthyshop/src/utils/router.js
+++ fe/healthyshop/src/utils/router.js
@@ -1,3 +1,4 @@
+export const ADMIN_PATH= "/quan-tri";
 export const ROUTERS={
     USER: {
         HOME: "",
@@ -6,5 +7,9 @@ export const ROUTERS={
         PRODUCT: "/san-pham/chi-tiet/:id",
         SHOPPING_CART: "/gio-hang",
         CHECKOUT: "/thanh-toan",
+    },
+    ADMIN: {
+        LOGIN: `${ADMIN_PATH}/dang-nhap`,
+       
     },
 }
