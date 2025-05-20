60540eb6843ca3a2f33967d44ff1c15f784a1a01
YtalyPham <nhuyphamdev@gmail.com>
2025-05-20 10:59:10 +0700

    handle quantity of shoppingcart while add product

--- fe/healthyshop/src/component/Quantity/index.js
+++ fe/healthyshop/src/component/Quantity/index.js
@@ -1,27 +1,43 @@
-import { memo } from "react";
+import { memo, useState } from "react";
 import "./style.scss";
 import useShoppingCart from "hooks/useShoppingCart";
 import { SESSION_KEY } from "utils/constant";
 import { ReactSession } from "react-client-session";
 const Quantity = ({ hasAddToCart = true, product }) => {
   const { addToCart } = useShoppingCart();
+  const [quantity,setQuantity] = useState(1);
+  //const curCart = ReactSession.get(SESSION_KEY.CART);
+  const incrementQuantity = (isPlus) =>{
+    if(!isPlus && quantity === 0){
+      return;
+    }
+    setQuantity(isPlus ? quantity + 1 : quantity - 1);
+  }
+  // Safely retrieve with optional chaining and default value
+  // const storedCart = JSON.parse(sessionStorage.getItem('__react_session__')) || {};
+  // const totalQuantity = storedCart.cart?.totalQuantity || 0;
 
+  // console.log("QuanTotal: ",totalQuantity); 
+
+  // const firstProductQuantity = storedCart.cart.products[0]?.quantity||0;
+  // console.log("QuantPerPro: ",firstProductQuantity);
+  
   return (
     <>
       <div className="quantity-container">
         <div className="quantity">
-          <span className="qtybtn">-</span>
-          <input type="number" defaultValue={1} />
-          <span className="qtybtn">+</span>
+          <span className="qtybtn" onClick={() => incrementQuantity(false)}>-</span>
+          <input type="number" value={quantity} readOnly/>
+          <span className="qtybtn" onClick={() => incrementQuantity(true)}>+</span>
         </div>
         {hasAddToCart && (
           <button
             type="button"
             className="button-submit"
             onClick={() => {
-              addToCart(product, 1);
+              addToCart(product, quantity);
               const curCart = ReactSession.get(SESSION_KEY.CART);
-              console.log(curCart);
+              
             }}
           >
             Thêm giỏ hàng
--- fe/healthyshop/src/index.js
+++ fe/healthyshop/src/index.js
@@ -4,9 +4,12 @@ import { BrowserRouter } from "react-router-dom";
 import RouterCustom from "./router";
 import "./style/style.scss";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
+import { SESSION_KEY } from "utils/constant";
+import { ReactSession } from "react-client-session";
 const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+ReactSession.setStoreType("sessionStorage");
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
